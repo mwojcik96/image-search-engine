3e0e3fc2e5cdca0986d7342c6ef5e1d24bf41120
mwojcik96 <96michal.wojcik@gmail.com>
2018-11-16 19:01:17 +0100

    added explenation file for simple query and some fixes in main

--- main.py
+++ main.py
@@ -13,55 +13,55 @@ def prepare_dataset():
         if filename.endswith(".jpeg") or filename.endswith(".jpg"):
             name_list.append(filename)
             image_list.append(Image(filename))
-    for featurename in os.listdir(features_dir):
-        if featurename.endswith(".dat"):
-            with open(os.path.join(features_dir, featurename)) as file:
+    for feat_name in os.listdir(features_dir):
+        if feat_name.endswith(".dat"):
+            with open(os.path.join(features_dir, feat_name)) as file:
                 deviations = None
                 for line in file:
                     splited_line = line.split()
                     if deviations is None:
                         deviations = np.empty((0, len(splited_line)-1))
                     image_list[name_list.index(splited_line[0])].fill_measure(
-                        os.path.splitext(featurename)[0], [float(it) for it in splited_line[1:]])
+                        os.path.splitext(feat_name)[0], [float(it) for it in splited_line[1:]])
                     deviations = np.append(deviations, [splited_line[1:]], axis=0)
                 deviations = np.std(deviations.astype(np.float), axis=0)
-                inv_std_devs[os.path.splitext(featurename)[0]] = 1/deviations
+                inv_std_devs[os.path.splitext(feat_name)[0]] = 1/deviations
     return image_list, name_list, inv_std_devs
 
 
-def simple_process(filename, featurename, imglist, namelist, invstddevs):
+def simple_process(filename, feature_name, images_list, namelist, inv_std_dev):
     index = namelist.index(filename)
-    query_image = imglist[index]
+    query_image = images_list[index]
     ranking = dict()
-    for img in imglist:
-        ranking[img.filename] = np.sum(np.multiply(invstddevs[featurename], np.absolute(
-                np.array(img.features[featurename]) - np.array(query_image.features[featurename]))))
+    for img in images_list:
+        ranking[img.filename] = np.sum(np.multiply(inv_std_dev[feature_name], np.absolute(
+                np.array(img.features[feature_name]) - np.array(query_image.features[feature_name]))))
     final_rank = sorted(ranking.items(), key=lambda kv: kv[0])
     _, max_val = max(final_rank, key=lambda kv: kv[1])
     final_rank = [(item[0], (max_val - item[1])/max_val) for item in final_rank]
     return final_rank
 
 
-def parse_query(query):
-    query = query.split()
-    if len(query) == 3:
-        return "max", [query[1]], "max", [query[2]]
+def parse_query(q):
+    q = q.split()
+    if len(q) == 3:
+        return "max", [q[1]], "max", [q[2]]
     else:
-        img_num = int(query[2])
+        img_num = int(q[2])
         img_start = 3
         img_stop = img_num + 3
         feat_num = img_stop + 1
         feat_start = feat_num + 1
-        return query[1], query[img_start:img_stop], query[img_stop], query[feat_start:]
+        return q[1], q[img_start:img_stop], q[img_stop], q[feat_start:]
 
 
-def calculate_score_for_query(images, names, inv_devs, image_op, image_list, feature_op, feature_list):
+def calculate_score_for_query(image_set, name_set, inv_std_deviations, image_op, image_list, feature_op, feature_list):
     whole_score = None
     for current_image in image_list:
 
         feature_score = None
         for current_feature in feature_list:
-            current_score = simple_process(current_image, current_feature, images, names, inv_devs)
+            current_score = simple_process(current_image, current_feature, image_set, name_set, inv_std_deviations)
             # print(current_image, current_feature, current_score)
 
             if feature_score is None:
